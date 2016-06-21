655002d49fded4001f29dd63fed99ea11f6dbb28
Nadeem Saiyed <nadeem.saiyed@cloud1990.com>
2016-06-21 21:23:51 +0530

    [REM] partner_extended : Removed Module

--- partner_extended/models/res_partner.py
+++
@@ -1,6 +0,0 @@
-from openerp import api, models, fields
-
-class res_partner(models.Model):
-
-    'state_id': fields.many2one("res.country.state", 'County', ondelete='restrict'),
-    'zip': fields.char('Postcode', size=24, change_default=True),
