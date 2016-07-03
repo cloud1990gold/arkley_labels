30570ba068cefe9d3e3b5bb7fec7e4f55ac0486f
Nadeem Saiyed <nadeem.saiyed@cloud1990.com>
2016-07-03 17:29:51 +0530

    [ADD] arkley_shop : Added Module

--- arkley_shop/__openerp__.py
+++ arkley_shop/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Product Extended",
+    'name': "Arkley Shop",
 
 
     'description': """
@@ -13,14 +13,15 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Sale',
+    'category': 'Ecommerce',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale'],
+    'depends': ['website_sale'],
 
     # always loaded
     'data': [
-        'views/product_attribute.xml',
+        # 'security/ir.model.access.csv',
+        'views/product_template.xml',
     ],
 }
